6aea1ee55b26ddc9dcce47a091fbb466e7c65251
minhtran <trcongminh.261002@gmail.com>
2025-03-24 17:07:40 +0700

    add authentication middleware for admin and user roles

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin.auth' => App\Admin\Http\Middleware\AdminAuthMiddleware::class,
+            'admin.login' => App\Admin\Http\Middleware\AdminLoginMiddleware::class,
+            'user.auth' => App\Http\Middleware\UserAuthMiddleware::class,
+            'user.login' => App\Http\Middleware\UserLoginMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
